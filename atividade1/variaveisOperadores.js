1bcd522ea4b99de747e5c26fec704c75e1228986
Stela alves <111458985+stelalves@users.noreply.github.com>
2024-08-23 01:26:16 -0300

    variaveisOperadores.js

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,7 @@
+var a = 8;
+var b = 16;
+
+console.log(`O resultado da soma entre ${a} e ${b} é igual a ${a+b}`);
+console.log(`O resultado da multiplicação entre ${a} e ${b} é igual a ${a*b}`);
+console.log(`O resultado da subtração entre ${a} e ${b} é igual a ${a-b}`);
+console.log(`O resultado da divisão entre ${a} e ${b} é igual a ${a/b}`);
